c1ffa8a06bedf276e8acae1926a646139627ccfd
Valter Lindberg <vlindbe@kth.se>
2025-01-28 12:10:22 +0100

    Minor fixes

--- SpringProgram/demo/src/main/java/com/example/demo/config/DatabaseLoader.java
+++ SpringProgram/demo/src/main/java/com/example/demo/config/DatabaseLoader.java
@@ -27,38 +27,7 @@ public class DatabaseLoader implements CommandLineRunner{
     @Override
     public void run(String... args) throws Exception {
         //We check if the roles exist, and if not we create them
-        if (roleRepository.findByName("recruiter") == null) {
-            Role role = new Role();
-            role.setId(1);
-            role.setName("recruiter");
-            roleRepository.save(role);
-        }
-        if (roleRepository.findByName("applicant") == null) {
-            Role role = new Role();
-            role.setId(2);
-            role.setName("applicant");
-            roleRepository.save(role);
-        }
-
-        //We then load the competences
-        if (competenceRepository.findByName("ticket sales")==null) {
-            Competence competence = new Competence();
-            competence.setId(1);
-            competence.setName("ticket sales");
-            competenceRepository.save(competence);
-        }
-        if (competenceRepository.findByName("lotteries")==null) {
-            Competence competence = new Competence();
-            competence.setId(2);
-            competence.setName("lotteries");
-            competenceRepository.save(competence);
-        }
-        if (competenceRepository.findByName("roller coaster operation")==null) {
-            Competence competence = new Competence();
-            competence.setId(3);
-            competence.setName("roller coaster operation");
-            competenceRepository.save(competence);
-        }
+        
 
     }
     
